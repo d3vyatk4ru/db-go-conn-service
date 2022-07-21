73a8499f4b31244b9d1f069bd7eb3b4349cbfcbf
d3vyatk4ru <danya.devyatkin@mail.ru>
2022-07-21 18:08:30 +0300

    add DELETE method and some fixes in resp in other methods

--- db_explorer.go
+++ db_explorer.go
@@ -53,15 +53,17 @@ func (h *Handler) TableList(w http.ResponseWriter, r *http.Request) {
 		tables += item + " "
 	}
 
+	resp := fmt.Sprintf("The tables in db: %v", tables)
+
+	log.Println(resp)
+
 	// send array of bytes to client
 	w.Write(
-		[]byte(
-			fmt.Sprintf("The tables in db: %v", tables),
-		),
+		[]byte(resp),
 	)
 }
 
-func (h *Handler) GetRowById(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) GetRecordById(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != "GET" {
 		http.Error(w, http.StatusText(405), 405)
@@ -97,13 +99,13 @@ func (h *Handler) GetRowById(w http.ResponseWriter, r *http.Request) {
 	err = h.
 		DB.
 		QueryRow(
-			fmt.Sprintf("USE [db_golang]; SELECT * FROM %s WHERE %s = %v", table, h.Table[idx].Id, id),
+			fmt.Sprintf("USE [db_golang]; SELECT * FROM %s WHERE %s = %v", h.Table[idx].Name, h.Table[idx].Id, id),
 		).
 		Scan(values...)
 
 	if err != nil {
 		log.Println(
-			fmt.Sprintf("[GetRowById] GET '/%v/%v'. Bad scanned to table %v.\n Error: %v", table, vars["id"], table, err.Error()),
+			fmt.Sprintf("[GetRowById] GET '/%v/%v'. Bad scanned to table %v.\n Error: %v", h.Table[idx].Name, vars["id"], table, err.Error()),
 		)
 		http.Error(w, err.Error(), 500)
 		return
@@ -119,12 +121,15 @@ func (h *Handler) GetRowById(w http.ResponseWriter, r *http.Request) {
 		},
 	)
 
+	log.Println(result)
+
 	w.Write(result)
 }
 
 func (h *Handler) tableHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "GET" {
+
 		table := mux.Vars(r)["table"]
 
 		cond, idx, err := contains(h.Table, table)
@@ -171,7 +176,7 @@ func (h *Handler) tableHandler(w http.ResponseWriter, r *http.Request) {
 
 		query := fmt.Sprintf(
 			"USE [db_golang]; SELECT * FROM %v ORDER BY %v OFFSET %d ROWS FETCH NEXT %d ROWS ONLY",
-			table, h.Table[idx].Id, o, l,
+			h.Table[idx].Name, h.Table[idx].Id, o, l,
 		)
 
 		log.Println(query)
@@ -220,6 +225,8 @@ func (h *Handler) tableHandler(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+
+		log.Println(data)
 
 		w.Write(result)
 	}
@@ -284,41 +291,11 @@ func (h *Handler) CreateRecord(w http.ResponseWriter, r *http.Request) {
 		),
 	)
 
-	id, _ := result.LastInsertId()
-
 	log.Println(
-		"Last insert id is:", id,
+		"Inserted: ", item,
 	)
 
 	return
-}
-
-func preprareInsertData(table TableInfo, r *http.Request) ([]interface{}, string, string) {
-
-	item := make([]interface{}, len(table.Fields))
-	columnForQuery := make([]string, len(table.Fields))
-	placeholder := make([]string, len(table.Fields))
-
-	for i, field := range table.Fields {
-
-		param := r.FormValue(field.Name)
-
-		if param != "" {
-			item[i] = param
-		} else {
-			switch field.Type {
-			case "int":
-				item[i] = 0
-			case "nvarchar", "text":
-				item[i] = ""
-			}
-		}
-
-		columnForQuery[i] = field.Name
-		placeholder[i] = fmt.Sprintf("@p%d", i+1)
-	}
-
-	return item, strings.Join(columnForQuery, ","), strings.Join(placeholder, ",")
 }
 
 func (h *Handler) UpdateRecord(w http.ResponseWriter, r *http.Request) {
@@ -383,17 +360,128 @@ func (h *Handler) UpdateRecord(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		log.Println(
-			fmt.Sprintf("[CreateRecord] Bad called RowsAffected()! \nError: %v", err.Error()),
+			fmt.Sprintf("[UpdateRecord] Bad called RowsAffected()! \nError: %v", err.Error()),
 		)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	resp := "Row affected " + fmt.Sprint(affected) + "\n" +
+		"Record with id = " + fmt.Sprint(id) + "was updated"
+
+	log.Println(resp)
+
 	w.Write(
-		[]byte(
-			"Row affected " + fmt.Sprint(affected),
-		),
+		[]byte(resp),
 	)
+}
+
+func (h *Handler) DeleteRecordById(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "DELETE" {
+		log.Println(
+			fmt.Sprintf("[DeleteRecordById] Bad HTTP Method. Need DELETE, got %v", r.Method),
+		)
+
+		http.Error(w, http.StatusText(405), 405)
+	}
+
+	vars := mux.Vars(r)
+
+	table := vars["table"]
+
+	cond, idx, err := contains(h.Table, table)
+
+	if !cond {
+		log.Println(
+			fmt.Sprintf("[DeleteRecordById] Bad table in endpoint! Table %v is not exist!\nError: %v", table, err.Error()),
+		)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	id, err := strconv.Atoi(vars["id"])
+
+	if err != nil {
+		log.Println(
+			fmt.Sprintf("[DeleteRecordById] DELETE '/%v/%v'. Bad converted id to int.\n Error: %v", table, vars["id"], err.Error()),
+		)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var NameID string = ""
+
+	for _, field := range h.Table[idx].Fields {
+
+		if field.IsKey {
+			NameID = field.Name
+		}
+
+	}
+
+	query := fmt.Sprintf(
+		"DELETE FROM %v WHERE %v = %d", h.Table[idx].Name, NameID, id,
+	)
+
+	log.Println(query)
+
+	result, err := h.DB.Exec(query)
+
+	if err != nil {
+		log.Println(
+			fmt.Sprintf("[DeleteRecordById] Bad Execute query! \nError: %v", err.Error()),
+		)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	affected, err := result.RowsAffected()
+
+	if err != nil {
+		log.Println(
+			fmt.Sprintf("[DeleteRecordById] Bad called RowsAffected()! \nError: %v", err.Error()),
+		)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resp := "Row affected " + fmt.Sprint(affected) + "\n" +
+		"Record with id = " + fmt.Sprint(id) + "was deleted"
+
+	log.Println(resp)
+
+	w.Write(
+		[]byte(resp),
+	)
+}
+
+func preprareInsertData(table TableInfo, r *http.Request) ([]interface{}, string, string) {
+
+	item := make([]interface{}, len(table.Fields))
+	columnForQuery := make([]string, len(table.Fields))
+	placeholder := make([]string, len(table.Fields))
+
+	for i, field := range table.Fields {
+
+		param := r.FormValue(field.Name)
+
+		if param != "" {
+			item[i] = param
+		} else {
+			switch field.Type {
+			case "int":
+				item[i] = 0
+			case "nvarchar", "text":
+				item[i] = ""
+			}
+		}
+
+		columnForQuery[i] = field.Name
+		placeholder[i] = fmt.Sprintf("@p%d", i+1)
+	}
+
+	return item, strings.Join(columnForQuery, ","), strings.Join(placeholder, ",")
 }
 
 func preprareUpdateQuery(table TableInfo, r *http.Request) (string, string, []interface{}) {
@@ -617,8 +705,6 @@ func GetTablesInfo(db *sql.DB) ([]TableInfo, error) {
 }
 
 func NewDBExplorer(db *sql.DB) (http.Handler, error) {
-	// тут вы пишете код
-	// обращаю ваше внимание - в этом задании запрещены глобальные переменные
 
 	handler := &Handler{
 		DB:   db,
@@ -640,10 +726,11 @@ func NewDBExplorer(db *sql.DB) (http.Handler, error) {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", handler.TableList).Methods("GET")
-	r.HandleFunc("/{table}/{id:[0-9]+}", handler.GetRowById).Methods("GET")
+	r.HandleFunc("/{table}/{id:[0-9]+}", handler.GetRecordById).Methods("GET")
 	r.HandleFunc("/{table}", handler.tableHandler).Methods("GET")
 	r.HandleFunc("/{table}", handler.CreateRecord).Methods("PUT")
 	r.HandleFunc("/{table}/{id:[0-9]+}", handler.UpdateRecord).Methods("POST")
+	r.HandleFunc("/{table}/{id:[0-9]+}", handler.DeleteRecordById).Methods("DELETE")
 
 	return r, nil
 }
