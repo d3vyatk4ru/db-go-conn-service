28afab915cb4f62585a1bd7d9cd6527f628d9cce
d3vyatk4ru <danya.devyatkin@mail.ru>
2022-10-24 18:39:58 +0300

    ms sql -> mysql

--- main.go
+++ main.go
@@ -1,58 +1,41 @@
+// тут лежит тестовый код
+// менять вам может потребоваться только коннект к базе
 package main
 
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 
-	_ "github.com/microsoft/go-mssqldb"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-// var (
-// // docker run -p 3306:3306 -v $(PWD):/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=golang -d mysql
-// // DSN = "root:pwd@tcp(localhost:1433)/golang?charset=utf8"
-// // DSN = "coursera:5QPbAUufx7@tcp(localhost:3306)/coursera?charset=utf8"
-// )
-
-const (
-	server   = "localhost"
-	user     = "sa"
-	port     = 1433
-	password = "super_PWD_go"
-	database = "master"
+var (
+	// DSN это соединение с базой
+	// вы можете изменить этот на тот который вам нужен
+	// docker run -p 3306:3306 -v $(PWD):/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=golang -d mysql
+	DSN = "root:1234@tcp(localhost:3306)/golang?charset=utf8"
+	// DSN = "coursera:5QPbAUufx7@tcp(localhost:3306)/coursera?charset=utf8"
 )
-
-// DSN это соединение с базой
-var DSN = fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
-	server, user, password, port, database)
 
 func main() {
-
-	db, err := sql.Open("sqlserver", DSN)
-
+	db, err := sql.Open("mysql", DSN)
 	if err != nil {
 		panic(err)
 	}
-
-	db.SetMaxOpenConns(10)
-
-	err = db.Ping() // тут будет первое подключение к бд
-
+	err = db.Ping() // вот тут будет первое подключение к базе
 	if err != nil {
 		panic(err)
 	}
 
 	handler, err := NewDBExplorer(db) //nolint:typecheck
-
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("starting server at :8082")
-
-	err = http.ListenAndServe(":8082", handler)
-
-	if err != nil {
-		panic(err)
+	if err := http.ListenAndServe(":8082", handler); err != nil {
+		log.Printf("error listenAndServer: %v", err)
 	}
 }
